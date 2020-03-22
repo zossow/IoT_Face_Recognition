dd5d17873f9232c93f51ff80f7cf25da4869d26a
tjadczak <tomasz.jadczak1996@gmail.com>
2020-03-22 21:11:31 +0100

    client app update

--- ssl_client.py
+++ ssl_client.py
@@ -40,7 +40,7 @@ class FaceRecognitionCameraApp(threading.Thread):
             print(datetime.datetime.now().strftime("%H:%M:%S"), "Thread-FaceRecognitionCameraApp: face_locations start")
             # face_locations = cascadeClassifier.detectMultiScale(image_gray)
             # face_locations = cascadeClassifier.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
-            face_locations = cascadeClassifier.detectMultiScale(image_gray, scaleFactor=1.5, minNeighbors=5, minSize=(20, 20), flags=cv2.CASCADE_SCALE_IMAGE)
+            face_locations = cascadeClassifier.detectMultiScale(image_gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
             print(datetime.datetime.now().strftime("%H:%M:%S"), "Thread-FaceRecognitionCameraApp: face_locations end")
 
             # Translate OpenCV coordinates from (x, y, w, h) to (top, right, bottom, left)
