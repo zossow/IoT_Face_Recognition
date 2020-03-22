f8a9f2beb3837b80e398dc20eaf822d704c3b1ed
tjadczak <tomasz.jadczak1996@gmail.com>
2020-03-22 10:26:50 +0100

    client app update

--- ssl_client.py
+++ ssl_client.py
@@ -18,9 +18,9 @@ class FaceRecognitionCameraApp(threading.Thread):
         # Load the known faces and embeddings along with OpenCV's Haar
         # cascade classifier for face detection
         with open(self.face_encodings_filepath, "rb") as file:
-            face_data = pickle.loads(file.read())
+            face_data = pickle.loads(file.read().decode('utf-8'))
         with open(self.haar_cascade_path, "rb") as file:
-            haar_data = file.read()
+            haar_data = file.read().decode('utf-8')
 
         stream = imutils.video.VideoStream(src=0, usePiCamera=False).start()
         # Load image from image file
