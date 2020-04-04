6671ffae9e170d4b3fb6a0f197775e2d8f5dfd7d
tjadczak <tomasz.jadczak1996@gmail.com>
2020-04-04 17:13:25 +0200

    client app update

--- test_client.py
+++ test_client.py
@@ -4,10 +4,8 @@ import time
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
     sock.bind(('0', 5000))
     sock.listen(5)
-    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-    with context.wrap_socket(sock, server_side=True) as ssock:
-        connection, address = ssock.accept()
-        ssock.setblocking(0)
-        while True:
-            print("Sleeping for 1s")
-            time.sleep(1)
+    connection, address = sock.accept()
+    sock.setblocking(0)
+    while True:
+        print("Sleeping for 1s")
+        time.sleep(1)
