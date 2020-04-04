8ef6f353363748a8527b91fca1f929370e22b140
tjadczak <tomasz.jadczak1996@gmail.com>
2020-04-04 16:29:41 +0200

    client app update

--- test_client.py
+++ test_client.py
@@ -4,7 +4,8 @@ import time
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
     sock.bind(('0', 500))
     sock.listen(5)
-    with ssl.wrap_socket(sock, server_side=True) as ssock:
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    with context.wrap_socket(sock, server_side=True) as ssock:
         connection, address = ssock.accept()
         ssock.setblocking(0)
         while True:
